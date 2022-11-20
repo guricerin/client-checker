417d62418c3e50d8e1b885080d340eefe93c6106
guricerin <chanbo1e9@gmail.com>
2022-11-20 22:02:07 +0900

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/client-checker/", // https://ja.vitejs.dev/guide/static-deploy.html#github-pages
   plugins: [svelte()]
 })
